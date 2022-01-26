be88a506f552e0ce6879344eb730f1f304846080
yustavia <98464788+yustavia@users.noreply.github.com>
2022-01-26 21:18:34 +0700

    Add files via upload

---
+++ tugasarray.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var names [4]string
+	names[0] = "nilai indeks 0"
+	names[1] = "nilai indeks 1"
+	names[2] = "nilai indeks 2"
+	names[3] = "nilai indeks 3"
+
+	fmt.Println(names[0])
+	fmt.Println(names[1])
+	fmt.Println(names[2])
+	fmt.Println(names[3])
+}
